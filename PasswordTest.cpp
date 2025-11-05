f2cd38450176187ed12361e95b951702d342d61f
igalaxy <hey@igalaxy.dev>
2025-11-05 11:45:01 -0800

    add unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,86 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, multi_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZZZ");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, single_number_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("1");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, multi_number_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("111");
+}
+
+TEST(PasswordTest, long_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zABCDEFG");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, long_number_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("123456");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, long_mixed_case_same_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zZABCDEFG");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, multi_char_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zzz");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, long_multi_char_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zzzABCDEFG");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, same_case_single_letter_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("zzzzzz");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, same_case_multi_letter_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("abcdefg");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case_single_letter_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("zZzZzZz");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, mixed_case_multi_letter_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aBcDeFg");
+	ASSERT_EQ(true, actual);
+}
